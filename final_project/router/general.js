d128d8b3f3e57fea215edd6b2c4adf38571bd264
iDmitryi <hdumitru1998@gmail.com>
2024-07-12 15:07:36 +0300

    4. get book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,8 +35,12 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+
+  const filtered_titles = Object.entries(books).filter(([isbn, val]) => {
+    return val.title === title
+  })
+  res.send(Object.fromEntries(filtered_titles))
 });
 
 //  Get book review
